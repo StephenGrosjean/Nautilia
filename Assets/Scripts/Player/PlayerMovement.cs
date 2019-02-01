6dd3ad30078131a30c5e2f218030a91b7b5d6453
Unknown <cavadiaethan@gmail.com>
2019-02-01 12:14:00 +0100

    did the first player/upgrade scripts
    Did the basic player movement, shooting and started the upgrade pick up

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float shootDelay = 1f;
+    private float _movementHorizontal;
+    private float _movementVertical;
+    private Rigidbody2D _playerRb;
+    
+    public float playerSpeed = 10;
+     
+     
+    private void Start()
+    {
+        _playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _movementHorizontal = Input.GetAxisRaw("Horizontal");
+        _movementVertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PlayerShoot();
+        }
+    } 
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(_movementHorizontal,_movementVertical);
+
+        _playerRb.velocity = movement * playerSpeed;
+    }
+
+    private void PlayerShoot()
+    {
+        shootDelay -= Time.deltaTime;
+        if (shootDelay <= 0)
+        {
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            shootDelay = 0.2f;
+        }
+    }
+}
---
+++ Assets/Scripts/Upgrades.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    [SerializeField] private float speedUpgrade = 4.0f ;
+    public PlayerMovement player;
+    
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            player.playerSpeed += speedUpgrade;
+            Destroy(gameObject);
+        }
+    }
+}
