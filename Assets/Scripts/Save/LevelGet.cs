14a062ff70da48c3e643176d02ecb7d1fe372e11
Stephen Grosjean <stephen-gm@hotmail.com>
2019-03-03 20:47:30 +0100

    Revert

--- Assets/Scripts/Save/LevelGet.cs
+++ Assets/Scripts/Save/LevelGet.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class LevelGet : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private XMLSave saveScript;
     private int level;
     public int Level {
@@ -24,17 +25,16 @@ public class LevelGet : MonoBehaviour
     }
 
     public void ResetSettings() {
-        AudioController.instance.PlaySound(AudioController.clips.ButtonClick);
         saveScript.dataBase.firstDB[1].value = 1;
         saveScript.Save();
-        GameObject.Find("VolumeSlider").GetComponent<VolumeSlider>().UpdateVolume();
     }
 
     public void ResetGame() {
-        AudioController.instance.PlaySound(AudioController.clips.ButtonClick);
         saveScript.dataBase.firstDB[0].value = 0;
         saveScript.Save();
     }
+
+    //text.text = "Level : " + level.ToString();
 }
 
 
